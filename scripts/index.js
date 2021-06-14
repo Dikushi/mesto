e4f9b21e9a09abc89f661659fc6aae81c69dd673
movchinnikov <dikushi@yandex.com>
2021-06-15 00:16:20 +0400

    add like listener/fixes

--- scripts/index.js
+++ scripts/index.js
@@ -6,12 +6,19 @@ let infoJob = content.querySelector('.profile__info-job');
 let inputName = content.querySelector('.popup__input_name');
 let inputJob = content.querySelector('.popup__input_job');
 let popupForm = content.querySelector('.popup__form');
-
+let infoLike = content.querySelectorAll('.elements__info-like');
+let editButton = content.querySelector('.profile__edit-button');
 
 popupClose.addEventListener('click', closePopup);
 popupForm.addEventListener('submit', formSubmit);
-document.querySelector('.profile__edit-button').addEventListener('click', editProfile);
+editButton.addEventListener('click', editProfile);
 
+// Слушаем каждый лайк на предмет нажатия
+for (let i = 0; i < infoLike.length; i++) {
+  infoLike[i].addEventListener('click', function () {
+    infoLike[i].classList.toggle('elements__info-like_active');
+  });
+}
 
 // Функция закрытия поп-апа
 function closePopup() {
