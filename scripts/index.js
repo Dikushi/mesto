3bdace9f10820db0898254e672314a9005311f3a
movchinnikov <dikushi@yandex.com>
2021-06-15 08:02:55 +0400

    fixes after review

--- scripts/index.js
+++ scripts/index.js
@@ -1,28 +1,21 @@
-let content = document.querySelector('.content');
+let content = document.querySelector('.root');
 let popupClose = content.querySelector('.popup__close');
 let popUp = content.querySelector('.popup');
 let infoName = content.querySelector('.profile__info-name');
 let infoJob = content.querySelector('.profile__info-job');
-let inputName = content.querySelector('.popup__input_name');
-let inputJob = content.querySelector('.popup__input_job');
+let inputName = content.querySelector('.popup__input-name');
+let inputJob = content.querySelector('.popup__input-job');
 let popupForm = content.querySelector('.popup__form');
-let infoLike = content.querySelectorAll('.elements__info-like');
 let editButton = content.querySelector('.profile__edit-button');
-
-popupClose.addEventListener('click', closePopup);
-popupForm.addEventListener('submit', formSubmit);
-editButton.addEventListener('click', editProfile);
-
-// Слушаем каждый лайк на предмет нажатия
-for (let i = 0; i < infoLike.length; i++) {
-  infoLike[i].addEventListener('click', function () {
-    infoLike[i].classList.toggle('elements__info-like_active');
-  });
-}
 
 // Функция закрытия поп-апа
 function closePopup() {
-  popUp.classList.remove('popup_opened')
+  popUp.classList.remove('popup_opened');
+}
+
+// Функция открытия поп-апа
+function openPopup() {
+  popUp.classList.add('popup_opened');
 }
 
 // Функция сохранения введенной информации в поп-апе
@@ -32,7 +25,7 @@ function formSubmit(e) {
   infoName.textContent = inputName.value;
   infoJob.textContent = inputJob.value;
 
-  popUp.classList.remove('popup_opened');
+  closePopup();
 }
 
 // Функция редактирования профиля
@@ -40,5 +33,9 @@ function editProfile() {
   inputName.value = infoName.textContent;
   inputJob.value = infoJob.textContent;
 
-  popUp.classList.add('popup_opened');
+  openPopup();
 }
+
+popupClose.addEventListener('click', closePopup);
+popupForm.addEventListener('submit', formSubmit);
+editButton.addEventListener('click', editProfile);
