a9be1a13f6bc9953e3a350f59dce5c72bf459e5f
movchinnikov <dikushi@yandex.com>
2021-08-25 00:03:44 +0400

    pr8

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
-import Card from '../scripts/components/Card.js';
-import FormValidator from '../scripts/components/FormValidator.js';
-import PopupWithForm from '../scripts/components/PopupWithForm.js';
-import PopupWithImage from '../scripts/components/PopupWithImage.js';
-import Section from '../scripts/components/Section.js';
-import UserInfo from '../scripts/components/UserInfo.js';
+import './pages/index.css';
+
+import Card from './scripts/components/Card.js';
+import FormValidator from './scripts/components/FormValidator.js';
+import PopupWithForm from './scripts/components/PopupWithForm.js';
+import PopupWithImage from './scripts/components/PopupWithImage.js';
+import Section from './scripts/components/Section.js';
+import UserInfo from './scripts/components/UserInfo.js';
 
 import {
   initialCards,
@@ -22,7 +24,7 @@ import {
   inputNamePhoto,
   inputLinkPhoto,
   cardsElements
-} from '../scripts/utils/constants.js'
+} from './scripts/utils/constants.js'
 
 // Обьект для управлением данными пользователя
 const userInfo = new UserInfo({
